a9c5f3d325dac1d86088a75be57d1f3638a00684
satojkovic <satojkovic@gmail.com>
2024-01-10 23:24:14 +0900

    Download gpt2-pytorch_model.bin if necessary

--- pytorch/gpt_pytorch.py
+++ pytorch/gpt_pytorch.py
@@ -2,15 +2,12 @@ import torch
 import argparse
 import sys
 import os
-from utils import load_encoder_hparams
+from utils import load_encoder_hparams_and_params
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--prompt", required=True, help="Input text")
-    parser.add_argument(
-        "--model_path", required=True, help="Path to gpt2-pytorch_model.bin"
-    )
     parser.add_argument(
         "--n_tokens_to_generate",
         default=40,
@@ -19,13 +16,11 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    state_dict = torch.load(args.model_path)
-    print(f"state_dict: {len(state_dict.keys())} params")
-
     model_size = "124M"
     models_dir = "models"
-    encoder, hparams = load_encoder_hparams(model_size, models_dir)
+    encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
     print("hparams:", hparams)
+    print("Num. of params:", len(params.keys()))
 
     print("prompt:", args.prompt)
     input_ids = encoder.encode(args.prompt)
--- pytorch/utils.py
+++ pytorch/utils.py
@@ -3,11 +3,30 @@ import json
 import sys
 import requests
 from tqdm import tqdm
+import torch
 
 # import picoGPT
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../picoGPT"))
 from encoder import get_encoder
+
+
+def save_file(file_path, req):
+    filename = os.path.basename(file_path)
+    with open(file_path, "wb") as f:
+        file_size = int(req.headers["content-length"])
+        chunk_size = 1000
+        with tqdm(
+            ncols=100,
+            desc="Fetching " + filename,
+            total=file_size,
+            unit_scale=True,
+            unit="b",
+        ) as pbar:
+            # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
+            for chunk in req.iter_content(chunk_size=chunk_size):
+                f.write(chunk)
+                pbar.update(chunk_size)
 
 
 # Copy from picoGPT because picoGPT/utils.py import tensorflow
@@ -26,31 +45,33 @@ def download_gpt2_files(model_size, model_dir):
         r = requests.get(f"{url}/{model_size}/{filename}", stream=True)
         r.raise_for_status()
 
-        with open(os.path.join(model_dir, filename), "wb") as f:
-            file_size = int(r.headers["content-length"])
-            chunk_size = 1000
-            with tqdm(
-                ncols=100,
-                desc="Fetching " + filename,
-                total=file_size,
-                unit_scale=True,
-                unit="b",
-            ) as pbar:
-                # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    f.write(chunk)
-                    pbar.update(chunk_size)
+        save_file(os.path.join(model_dir, filename), r)
 
 
-def load_encoder_hparams(model_size, models_dir):
-    assert model_size in ["124M", "355M", "774M", "1558M"]
+def download_pyth_gpt2_file(pyth_model_path):
+    pyth_model_filename = os.path.basename(pyth_model_path)
+    base_url = "https://s3.amazonaws.com/models.huggingface.co/bert"
+    r = requests.get(f"{base_url}/{pyth_model_filename}", stream=True)
+    r.raise_for_status()
+
+    save_file(pyth_model_path, r)
+
+
+def load_encoder_hparams_and_params(model_size, models_dir):
+    # 124M only in pytorch
+    assert model_size in ["124M"]
 
     model_dir = os.path.join(models_dir, model_size)
-    if not model_dir:  # download files if necessary
+    if not os.path.exists(model_dir):  # download files if necessary
         os.makedirs(model_dir, exist_ok=True)
         download_gpt2_files(model_size, model_dir)
 
+    pyth_model_path = os.path.join(models_dir, "gpt2-pytorch_model.bin")
+    if not os.path.exists(pyth_model_path):
+        download_pyth_gpt2_file(pyth_model_path)
+
     encoder = get_encoder(model_size, models_dir)
     hparams = json.load(open(os.path.join(model_dir, "hparams.json")))
+    params = torch.load(pyth_model_path)
 
-    return encoder, hparams
+    return encoder, hparams, params
